48480d936b442b68ceaf267504136d5a89a9254a
Brena <brenaennet@gmail.com>
2023-02-16 11:58:40 -0300

    OData json

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
 	"../model/formatter"
-], function(Controller, formatter) {
+], function(Controller, JSONModel, formatter) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.basicTemplate.controller.Home", {
@@ -10,6 +11,57 @@ sap.ui.define([
 
 		onInit: function () {
 
+			//create a date object
+			var oData = {
+				"title": "My orders",
+				"SalesOrderSet": [{
+					"SalesOrderID": "0001",
+					"CustomerName": "Apple",
+					"LifecycleStatusDescription": "Completed",
+					"LifecycleStatus": "Success",
+					"GrossAmount": "199",
+					"CurrencyCode": "USD"
+				  }, {
+					"SalesOrderID": "0002",
+					"CustomerName": "Google",
+					"LifecycleStatusDescription": "Completed",
+					"LifecycleStatus": "Success",
+					"GrossAmount": "277",
+					"CurrencyCode": "USD"
+				  }, {
+					"SalesOrderID": "0003",
+					"CustomerName": "Amazon",
+					"LifecycleStatusDescription": "Error",
+					"LifecycleStatus": "Error",
+					"GrossAmount": "599",
+					"CurrencyCode": "USD"
+				  }]
+				}
+			//JSONModel instance
+			var oModel = new JSONModel(oData);
+			//ESTE É UM EXEMPLO DE QUE QUAQUER ALTERAÇÃO REFLETIDA NA MINHA MODEL NÃO RETORNARÁ NOVAMENTE POR SER UMA LIGAÇÃO UNILATERAL.
+			oModel.setDefaultBindingMode("OneWay");
+			//Assign the model to view
+			this.getView().setModel(oModel);
+		},
+		onExit: function () {
+			// alert("onExit called");
+		},
+		onBeforeRendering: function () {
+			// alert("onBeforeRendering called");
+		},
+		onAfterRendering: function () {
+			// alert("onAfterRendering called");
+		},
+		onGroup: function () {
+			alert("onGroup called");
+			this.getView().destroy();
+		},
+		onSort: function () {
+			alert("onSort called");
+		},
+		onSearch: function () {
+			alert("onSearch called");
 		}
 	});
 });
