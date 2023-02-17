9be6b54868def48c9fe26c052a5cf7c7ec7a4fb0
Brena <brenaennet@gmail.com>
2023-02-17 11:33:03 -0300

    Adicionando um OData

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -2,47 +2,47 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
 	"../model/formatter"
-], function(Controller, JSONModel, formatter) {
+], function(Controller, JSONModel, Formatter) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.basicTemplate.controller.Home", {
 
-		formatter: formatter,
+		formatter: Formatter,
 
 		onInit: function () {
 
 			//create a date object
-			var oData = {
-				"title": "My orders",
-				"SalesOrderSet": [{
-					"SalesOrderID": "0001",
-					"CustomerName": "Apple",
-					"LifecycleStatusDescription": "Completed",
-					"LifecycleStatus": "Success",
-					"GrossAmount": "199",
-					"CurrencyCode": "USD"
-				  }, {
-					"SalesOrderID": "0002",
-					"CustomerName": "Google",
-					"LifecycleStatusDescription": "Completed",
-					"LifecycleStatus": "Success",
-					"GrossAmount": "277",
-					"CurrencyCode": "USD"
-				  }, {
-					"SalesOrderID": "0003",
-					"CustomerName": "Amazon",
-					"LifecycleStatusDescription": "Error",
-					"LifecycleStatus": "Error",
-					"GrossAmount": "599",
-					"CurrencyCode": "USD"
-				  }]
-				}
-			//JSONModel instance
-			var oModel = new JSONModel(oData);
-			//ESTE É UM EXEMPLO DE QUE QUAQUER ALTERAÇÃO REFLETIDA NA MINHA MODEL NÃO RETORNARÁ NOVAMENTE POR SER UMA LIGAÇÃO UNILATERAL.
-			oModel.setDefaultBindingMode("OneWay");
-			//Assign the model to view
-			this.getView().setModel(oModel);
+			// var oData = {
+			// 	"title": "My orders",
+			// 	"SalesOrderSet": [{
+			// 		"SalesOrderID": "0001",
+			// 		"CustomerName": "Apple",
+			// 		"LifecycleStatusDescription": "Completed",
+			// 		"LifecycleStatus": "Success",
+			// 		"GrossAmount": "199",
+			// 		"CurrencyCode": "USD"
+			// 	  }, {
+			// 		"SalesOrderID": "0002",
+			// 		"CustomerName": "Google",
+			// 		"LifecycleStatusDescription": "Completed",
+			// 		"LifecycleStatus": "Success",
+			// 		"GrossAmount": "277",
+			// 		"CurrencyCode": "USD"
+			// 	  }, {
+			// 		"SalesOrderID": "0003",
+			// 		"CustomerName": "Amazon",
+			// 		"LifecycleStatusDescription": "Error",
+			// 		"LifecycleStatus": "Error",
+			// 		"GrossAmount": "599",
+			// 		"CurrencyCode": "USD"
+			// 	  }]
+			// 	}
+			// //JSONModel instance
+			// var oModel = new JSONModel(oData);
+			// //ESTE É UM EXEMPLO DE QUE QUAQUER ALTERAÇÃO REFLETIDA NA MINHA MODEL NÃO RETORNARÁ NOVAMENTE POR SER UMA LIGAÇÃO UNILATERAL.
+			// oModel.setDefaultBindingMode("OneWay");
+			// //Assign the model to view
+			// this.getView().setModel(oModel);
 		},
 		onExit: function () {
 			// alert("onExit called");
@@ -63,5 +63,6 @@ sap.ui.define([
 		onSearch: function () {
 			alert("onSearch called");
 		}
+	
 	});
 });
